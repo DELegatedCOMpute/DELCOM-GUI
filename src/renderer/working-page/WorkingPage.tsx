664815a2a6b5207a0d14f32e8485cb23abd580ae
Jack Truxal <jtruxal@gatech.edu>
2024-04-15 10:19:49 -0400

    styling changes and changed the RAM estimation to ceiling

--- src/renderer/working-page/WorkingPage.tsx
+++ src/renderer/working-page/WorkingPage.tsx
@@ -4,7 +4,7 @@ import {
   Container,
   Typography,
   Box,
-  Grid
+  Grid,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './WorkingPage.css';
@@ -12,37 +12,41 @@ import './WorkingPage.css';
 export default function WorkerPage() {
   return (
     <Grid
-  container
-  spacing={0}
-  direction="column"
-  justifyContent="center"
-  sx={{ minHeight: '100vh' }}
-  >
-    <Container maxWidth="sm" sx={{ marginTop: 4 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: 2,
-        }}
-      >
-        <Typography variant="h4" align="center" paddingBottom={10}>You've successfully joined the DelCom workforce!</Typography>
-        <LinearProgress style={{ width: '100%' }} />
-        <Typography variant="h6" paddingBottom={5}>Waiting For Job</Typography>
-        <Link to="/" style={{ textDecoration: 'none', width: '100%' }}>
-          <Button
-            variant="contained"
-            fullWidth
-            onClick={async () => {
-              window.electron.ipcRenderer.leaveWorkForce();
-            }}
-          >
-            Quit Working
-          </Button>
-        </Link>
-      </Box>
-    </Container>
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}
+    >
+      <Container maxWidth="sm" sx={{ marginTop: 4 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 2,
+          }}
+        >
+          <Typography variant="h4" align="center" paddingBottom={10}>
+            You've successfully joined the DelCom workforce!
+          </Typography>
+          <LinearProgress style={{ width: '100%' }} />
+          <Typography variant="h6" paddingBottom={5}>
+            Waiting For Job
+          </Typography>
+          <Link to="/" style={{ textDecoration: 'none', width: '100%' }}>
+            <Button
+              variant="contained"
+              fullWidth
+              onClick={async () => {
+                window.electron.ipcRenderer.leaveWorkForce();
+              }}
+            >
+              Quit Working
+            </Button>
+          </Link>
+        </Box>
+      </Container>
     </Grid>
   );
 }
